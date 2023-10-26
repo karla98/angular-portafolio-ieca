b701cafe4e42854c89b6ea8eb34f6868c19c6c8d
karla_rios <riosdiazkm@gmail.com>
2023-10-26 00:15:10 -0600

    Javascript -  Ejercicio 4: Ecmascript

--- javascript/4-ecmascript/ejercicio.js
+++ javascript/4-ecmascript/ejercicio.js
@@ -1,5 +1,5 @@
 // AUXILIDADORES DE ECMAScript
-// Utiliza los auxilidadores de ECMAScript dentro de las funciones (numerosMayoresAMedia & ordenamientoMayorAMenor) de la clase ListaNumeros 
+// Utiliza los auxilidadores de ECMAScript dentro de las funciones (numerosMayoresAMedia & ordenamientoMayorAMenor) de la clase ListaNumeros
 // para obtener el siguiente resultado
 
 /*
@@ -9,45 +9,52 @@
     [10,9,8,7,6,5,4,3,2,1] // proviene de la función ordenamientoMayorAMenor
  */
 
-
 class ListaNumeros {
-    constructor(numeros) {
-        this._numeros = numeros
-    }
+  constructor(numeros) {
+    this._numeros = numeros;
+  }
 
-    get numeros() {
-        return this._numeros
-    }
+  get numeros() {
+    return this._numeros;
+  }
 
-    numerosMayoresAMedia() {
-        /*
-            filter. Filtrar items
-            Se utiliza para crear un nuevo array reteniendo solo los elementos que cumplan con una condición específica
-            La condición debe devolver `true` o `false`
-        */
-       
-        // Calculo de la media
-        const media = this.numeros.reduce((a, b) => a + b, 0) / this.numeros.length
+  numerosMayoresAMedia() {
+    /*
+        filter. Filtrar items
+        Se utiliza para crear un nuevo array reteniendo solo los elementos que cumplan con una condición específica
+        La condición debe devolver `true` o `false`
+    */
 
-        // COMPLETA AQUÍ CÓDIGO CON MÉTODO .filter
-    }
+    // Calculo de la media
+    const media = this.numeros.reduce((a, b) => a + b, 0) / this.numeros.length;
 
-    ordenamientoMayorAMenor() {
-        /*
-            sort. Ordenamiento condicionado
-            Investiga más sobre el funcionamiento de los parámetros (a,b) de sort para que lograr 
-            un ordenamiento de mayor a menor
+    // COMPLETA AQUÍ CÓDIGO CON MÉTODO .filter
+    const numerosMayores = this.numeros.filter(function (numero) {
+      return numero > media;
+    });
 
-            this.numeros.sort((a, b))
-        */
+    return console.log(numerosMayores);
+  }
 
-        // COMPLETA AQUÍ CÓDIGO CON MÉTODO .sort
+  ordenamientoMayorAMenor() {
+    /*
+        sort. Ordenamiento condicionado
+        Investiga más sobre el funcionamiento de los parámetros (a,b) de sort para que lograr 
+        un ordenamiento de mayor a menor
 
-        console.log(this.numeros)
-    }
-    
+        this.numeros.sort((a, b))
+    */
+
+    // COMPLETA AQUÍ CÓDIGO CON MÉTODO .sort
+    //this.numeros.sort((a, b) => b - a);
+    this.numeros.sort((a, b) => {
+        return b - a;
+    });
+
+    console.log(this.numeros);
+  }
 }
 
-const misNumeros = new ListaNumeros([1,5,3,4,2,6,8,10,9,7])
-misNumeros.numerosMayoresAMedia()
-misNumeros.ordenamientoMayorAMenor()
+const misNumeros = new ListaNumeros([1, 5, 3, 4, 2, 6, 8, 10, 9, 7]);
+misNumeros.numerosMayoresAMedia();
+misNumeros.ordenamientoMayorAMenor();
